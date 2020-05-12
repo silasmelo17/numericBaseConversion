f899fdcdf3336c94df89b337d1b263a25a7d5d6b
Silas de Melo Araujo <silasmelo1967@gmail.com>
2020-05-12 20:49:33 -0300

    create layout

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './global.css'
 
 function App() {
-  return ( <h1>Start project</h1> )
+  const [ error, setError ] = useState('')
+  const [ result, setResult ] = useState('')
+
+  return (
+    <>
+      <svg viewBox=' 0, 0, 1600, 450' className='background-curve' >
+        <path d="M 0 0 L 1600 0 L 1600 450 Q 1270 140 820 280 Q 410 450 0 290 Z" />
+      </svg>
+
+      <h1 className='title'>Conversão de bases</h1>
+      <section className='container' >
+        <form>
+          <p>{error}</p>
+
+          <section className='row'>
+            <select>
+              <option selected >Binário</option>
+              <option>Octagonal</option>
+              <option>Decimal</option>
+              <option>Hexadecimal</option>
+              <option>ASCII</option>
+            </select>
+
+            <span>PARA</span>
+
+            <select>
+              <option>Binário</option>
+              <option>Octagonal</option>
+              <option selected >Decimal</option>
+              <option>Hexadecimal</option>
+              <option>ASCII</option>
+            </select>
+          </section> 
+
+          <textarea rows={5} ></textarea>
+          <p>{result}</p>
+          <input type='button' value='convert' />  
+        </form>
+      </section>
+    </>
+  )
 }
 
 export default App;
