c74b59e4450ff5b9894af8479b2bce3a6fcd8925
Silas de Melo Araujo <silasmelo1967@gmail.com>
2020-05-17 01:01:39 -0300

    Updates

---
+++ precache-manifest.fcbc0efcdc7da8595a73bbf639239717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2897df6f88781975a488a6be882ec18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c3398a19a57de68395d",
+    "url": "/static/css/main.132e07f3.chunk.css"
+  },
+  {
+    "revision": "da87c50f58d91b531807",
+    "url": "/static/js/2.84cbe5ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84cbe5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3398a19a57de68395d",
+    "url": "/static/js/main.72031406.chunk.js"
+  },
+  {
+    "revision": "f26ba8da7c41e270b6e8",
+    "url": "/static/js/runtime-main.c4f1d106.js"
+  }
+]);
