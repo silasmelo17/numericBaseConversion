1fbd0d8fda285ec84aabe285676c71992a140c66
Silas de Melo Araujo <silasmelo1967@gmail.com>
2020-05-17 00:23:03 -0300

    created layout

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,45 +1,78 @@
 import React, { useState } from 'react';
+import { stringToArray, conversionToDecimal, decimalToBase, validateNumberToConvert } from './conversion'
 
 import './global.css'
 
 function App() {
-  const [ error, setError ] = useState('')
-  const [ result, setResult ] = useState('')
+  const [ result, setResult ] = useState('Initialize')
+  const [ fromBase, setFromBase ] = useState('binary')
+  const [ toBase, setToBase ] = useState( 'decimal' )
+  const [ userInput, setUserInput ] = useState('')
+
+  function handleValidateUserInput ( event ) {
+    const isPermitted = validateNumberToConvert( event.key, fromBase )
+    if ( !isPermitted )
+      event.preventDefault()
+  }
+
+  function handleConversion () {
+    const inputCharacters = stringToArray( userInput )
+    const decimal = conversionToDecimal( inputCharacters, fromBase )
+    const numberConverted = decimalToBase( decimal, toBase )
+
+    setResult( numberConverted )
+  }
 
   return (
     <>
-      <svg viewBox=' 0, 0, 1600, 450' className='background-curve' >
+      <svg width="1600" height="450" className='background-curve' >
         <path d="M 0 0 L 1600 0 L 1600 450 Q 1270 140 820 280 Q 410 450 0 290 Z" />
       </svg>
 
       <h1 className='title'>Conversão de bases</h1>
       <section className='container' >
         <form>
-          <p>{error}</p>
 
           <section className='row'>
-            <select>
-              <option selected >Binário</option>
-              <option>Octagonal</option>
-              <option>Decimal</option>
-              <option>Hexadecimal</option>
-              <option>ASCII</option>
+            <select 
+              defaultValue={fromBase}
+              onChange={ event => setFromBase( event.target.value ) } 
+            >
+              <option value='binary' >Binário</option>
+              <option value='octagonal' >Octagonal</option>
+              <option value='decimal' >Decimal</option>
+              <option value='hexadecimal' >Hexadecimal</option>
+              <option value='ascii'>ASCII</option>
             </select>
 
             <span>PARA</span>
 
-            <select>
-              <option>Binário</option>
-              <option>Octagonal</option>
-              <option selected >Decimal</option>
-              <option>Hexadecimal</option>
-              <option>ASCII</option>
+            <select 
+              defaultValue={toBase}
+              onChange={ event => setToBase( event.target.value ) } 
+            >
+              <option value='binary' >Binário</option>
+              <option value='octagonal' >Octagonal</option>
+              <option value='decimal' >Decimal</option>
+              <option value='hexadecimal' >Hexadecimal</option>
+              <option value='ascii'>ASCII</option>
             </select>
           </section> 
 
-          <textarea rows={5} ></textarea>
-          <p>{result}</p>
-          <input type='button' value='convert' />  
+          <textarea 
+            rows={5}
+            value={userInput}
+            onChange={ event => setUserInput( event.target.value ) } 
+            onKeyPress={ handleValidateUserInput }
+          ></textarea>
+          
+          <p className='result' >{result}</p>
+          
+          <input 
+            type='button' 
+            value='convert' 
+            onClick={handleConversion} 
+          />  
         </form>
       </section>
     </>
