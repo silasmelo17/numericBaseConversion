16e7fd42dd3536c594ff35d10f778f759d268e59
Silas de Melo Araujo <silasmelo1967@gmail.com>
2020-08-17 21:14:11 -0300

    refactored

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,81 +1,83 @@
 import React, { useState, useEffect } from 'react';
-import { stringToArray, conversionToDecimal, decimalToBase, validateNumberToConvert } from './conversion'
+import { validateInput, convertToBase } from './conversion'
 
 import './global.css'
 import './responsive.css'
 
 function App() {
   const [ result, setResult ] = useState('')
-  const [ fromBase, setFromBase ] = useState('binary')
-  const [ toBase, setToBase ] = useState( 'decimal' )
+  const [ fromBase, setFromBase ] = useState(2)
+  const [ toBase, setToBase ] = useState(10)
   const [ userInput, setUserInput ] = useState('')
 
   function handleValidateUserInput ( event ) {
-    const isPermitted = validateNumberToConvert( event.key, fromBase )
-    if ( !isPermitted )
+    const isPermitted = validateInput(event.key, fromBase)
+    if(!isPermitted)
       event.preventDefault()
   }
 
   function handleConversion () {
-    const inputCharacters = stringToArray( userInput )
-    const decimal = conversionToDecimal( inputCharacters, fromBase )
-    const numberConverted = decimalToBase( decimal, toBase )
-
-    setResult( numberConverted )
+    const convert = convertToBase( userInput, fromBase, toBase )
+    setResult(convert)
   }
 
-  return (
-    <>
-      <svg className='background-curve' >
-        <path d="M 1 1 L 1 0 L 0 0 L 0 1 Q 0.02 0.98 0.1 0.96 Q 0.16 0.96 0.22 0.98 Q 0.3 1 0.36 0.98 Q 0.46 0.54 0.96 Q 0.64 1 0.72 0.98 Q 0.8 0.96 0.88 0.96 Q 0.96 0.96 1 1 Z" />
-      </svg>
+  const handleChangeOption = setValue => event => {
+    setUserInput('')
+    setValue( event.target.value )
+  }
 
-      <h1 className='title'>Conversão de bases</h1>
-      <section className='container' >
-        <form>
-
-          <section className='row'>
-            <select 
-              defaultValue={fromBase}
-              onChange={ event => setFromBase( event.target.value ) } 
-            >
-              <option value='binary' >Binário</option>
-              <option value='octagonal' >Octagonal</option>
-              <option value='decimal' >Decimal</option>
-              <option value='hexadecimal' >Hexadecimal</option>
-            </select>
-
-            <span>PARA</span>
-
-            <select 
-              defaultValue={toBase}
-              onChange={ event => setToBase( event.target.value ) } 
-            >
-              <option value='binary' >Binário</option>
-              <option value='octagonal' >Octagonal</option>
-              <option value='decimal' >Decimal</option>
-              <option value='hexadecimal' >Hexadecimal</option>
-            </select>
-          </section> 
-
-          <textarea 
-            rows={5}
-            value={userInput}
-            onChange={ event => setUserInput( event.target.value ) } 
-            onKeyPress={ handleValidateUserInput }
-          ></textarea>
-          
-          <p>{result}</p>
-          
-          <input 
-            type='button' 
-            value='convert' 
-            onClick={handleConversion} 
-          />  
-        </form>
-      </section>
-    </>
+  const BaseSelect = ({ value, setValue }) => (
+    <select 
+      defaultValue={value}
+      onChange={handleChangeOption(setValue)} 
+    >
+      <option value={2} >Binário</option>
+      <option value={8} >Octagonal</option>
+      <option value={10} >Decimal</option>
+      <option value={16} >Hexadecimal</option>
+    </select>
   )
+
+  const CreateCurve = () => (
+    <svg class='curve'>
+      <defs>
+        <clipPath id='curve' clipPathUnits="objectBoundingBox" >
+          <path d='M 1 0 L 0 0 L 0 0.5 Q .15 .45 .3 .475 Q .45 .5 .55 .475 Q .7 .45 .8 .475 Q .9 .5 1 .5 Z' />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+
+  return (<>
+    <CreateCurve />
+
+    <h1 className='title'>Conversão de bases</h1>
+    <section className='container' >
+      <form>
+
+        <section className='row'>
+          <BaseSelect value={fromBase} setValue={setFromBase} />
+          <span>PARA</span>
+          <BaseSelect value={toBase} setValue={setToBase} />
+        </section> 
+
+        <textarea 
+          rows={5}
+          value={userInput}
+          onChange={ event => setUserInput( event.target.value ) } 
+          onKeyPress={ handleValidateUserInput }
+        ></textarea>
+        
+        <p>{result}</p>
+        
+        <input 
+          type='button' 
+          value='convert' 
+          onClick={handleConversion} 
+        />  
+      </form>
+    </section>
+  </>)
 }
 
 export default App;
